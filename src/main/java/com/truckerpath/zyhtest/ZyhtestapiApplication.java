6a9c8c1553e07f0610224e60e80293e6853db2b4
yanheng zhang <907311201@qq.com>
2018-12-07 16:51:02 +0800

    add property

--- src/main/java/com/truckerpath/zyhtest/ZyhtestapiApplication.java
+++ src/main/java/com/truckerpath/zyhtest/ZyhtestapiApplication.java
@@ -1,16 +1,13 @@
 package com.truckerpath.zyhtest;
 
-import javax.servlet.MultipartConfigElement;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 @EnableCaching
 @EnableScheduling
 public class ZyhtestapiApplication {
@@ -19,14 +16,5 @@ public class ZyhtestapiApplication {
         SpringApplication.run(ZyhtestapiApplication.class, args);
     }
     
-    @Bean
-    public MultipartConfigElement multipartConfigElement() {
-        MultipartConfigFactory factory = new MultipartConfigFactory();
-        //单个文件最大 
-        factory.setMaxFileSize("200MB"); //KB,MB
-        //设置总上传数据总大小
-        factory.setMaxRequestSize("500MB");
-        return factory.createMultipartConfig();
-    }
     
 }
