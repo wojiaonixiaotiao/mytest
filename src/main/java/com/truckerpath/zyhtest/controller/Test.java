100727d17a3af85fe0ae6433e0571441efc1c240
yanheng zhang <907311201@qq.com>
2018-12-07 17:10:45 +0800

    add controller

---
+++ src/main/java/com/truckerpath/zyhtest/controller/Test.java
@@ -0,0 +1,12 @@
+package com.truckerpath.zyhtest.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Test {
+	@RequestMapping(value = "tp/trucksetting", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
+	public void myTest() {
+	}
+}
