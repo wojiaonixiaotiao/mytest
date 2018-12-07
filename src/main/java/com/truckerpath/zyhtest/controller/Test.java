408dea923dcf8424f572a8fd0044dff48cb492cd
yanheng zhang <907311201@qq.com>
2018-12-07 17:18:46 +0800

    test

--- src/main/java/com/truckerpath/zyhtest/controller/Test.java
+++ src/main/java/com/truckerpath/zyhtest/controller/Test.java
@@ -1,12 +1,16 @@
 package com.truckerpath.zyhtest.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Test {
-	@RequestMapping(value = "tp/trucksetting", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
-	public void myTest() {
+	@Value("${tpbucket}")
+	private String bucket = "truckloads-files-test";
+	@RequestMapping(value = "forum/test", method = RequestMethod.GET)
+	public String myTest() {
+		return bucket;
 	}
 }
